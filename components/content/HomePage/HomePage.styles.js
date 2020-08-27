aeb1f963908aa7c6b14e13cda32a555c98bcc828
szymonobonski <sz.obonski@gmail.com>
2020-08-27 17:55:19 +0200

    Add base for homepage

---
+++ components/content/HomePage/HomePage.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledSection = styled.section``;
+const StyledHeading = styled.h1``;
+const StyledSectionTitle = styled.h2``;
+const StyledBlock = styled.div``;
+const StyledBlockNumber = styled.span``;
+const StyledBlockText = styled.p``;
+
+export {
+  StyledSection,
+  StyledHeading,
+  StyledSectionTitle,
+  StyledBlock,
+  StyledBlockNumber,
+  StyledBlockText,
+};
--- components/content/HomePage/index.tsx
+++ components/content/HomePage/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import {
+  StyledSection,
+  StyledHeading,
+  StyledSectionTitle,
+  StyledBlock,
+  StyledBlockNumber,
+  StyledBlockText,
+} from './HomePage.styles';
+
+// const steps = [''];
+
 const HomePage = () => (
-  <div>
-    HomePage
-  </div>
+  <>
+    <StyledSection>
+      <StyledHeading>
+        <span>Try.</span>
+        <span>Fail.</span>
+        <span>Retake.</span>
+        <span>Succeed.</span>
+      </StyledHeading>
+    </StyledSection>
+    <StyledSection>
+      <StyledSectionTitle>How it works?</StyledSectionTitle>
+      <StyledBlock>
+        <StyledBlockNumber>
+          1
+        </StyledBlockNumber>
+        <StyledBlockText>
+          123
+        </StyledBlockText>
+      </StyledBlock>
+    </StyledSection>
+  </>
 );
 
 export default HomePage;
