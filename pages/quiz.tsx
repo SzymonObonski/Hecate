af3af1f579f3c46570ddf1a27cd3f23f72dd8776
szymonobonski <sz.obonski@gmail.com>
2020-09-03 19:49:15 +0200

    Add default query

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Layout } from '@components/molecules';
 
-const Quiz = () => (
-  <Layout>
-    Quiz
-  </Layout>
-);
+const Quiz = () => {
+  useEffect(() => {
+    fetch('/api/quiz', {
+      method: 'POST',
+      mode: 'cors', // no-cors, *cors, same-origin
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ params: [{ tech: 'JavaScript', level: 'middle' }, { tech: 'React', level: 'middle' }] }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
+  return (
+    <Layout>
+      Quiz
+    </Layout>
+  );
+};
 export default Quiz;
